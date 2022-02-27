7889e21f5922860f6ee0acf30c3819451386dfe4
Triston Claydon <t_claydonhotmail>
2022-02-27 08:08:10 -0800

    first commit

---
+++ src/main/java/aetna/come/aetnahealthcare/AetnaHealthcareApplication.java
@@ -0,0 +1,13 @@
+package aetna.come.aetnahealthcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AetnaHealthcareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AetnaHealthcareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aetna/come/aetnahealthcare/AetnaHealthcareApplicationTests.java
@@ -0,0 +1,13 @@
+package aetna.come.aetnahealthcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AetnaHealthcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
